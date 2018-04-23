13d4b9e346f859705c7b0df00aedd7b92a31765d
lixiaolong <lixiaolong@administratordeMacBook-Pro.local>
2018-04-23 17:46:53 +0800

    user

--- app/router.js
+++ app/router.js
@@ -5,5 +5,5 @@
  */
 module.exports = app => {
   const { router, controller } = app;
-  router.get('/index', controller.home.index);
+  router.get('/user/getalluser', controller.user.getAllUser);
 };
